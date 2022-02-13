6253f37fb204a468278ef80b64bbef7bd8e9ab5d
megaboi2005 <57996646+megaboi2005@users.noreply.github.com>
2022-02-13 08:54:30 -0600

    start small, get big

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+from aiohttp import web
 
+
+def form(name,desc,location):
+	form = open('elements/form.txt','r')
+	formout = form.read()
+	return formout.replace('{name}',name).replace('{location}',location).replace('{desc}',desc)
+
+def filter(var):
+	output = var.replace('"','\"').replace('<',' &lt').replace('>','&gt;').replace('</textarea>','(nice try)').replace("'",'\'')
+	return output
+
+async def register(request):
+	try:
+		name = request.rel_url.query['name']
+		password = request.rel_url.query['pass']
+		finalname = {filter(name)}
+		finalpass = filter(password)
+		output = '{"name":"'+finalname+'","post":"'+finalpass+'"}'
+		return web.Response(text='yay',content_type='text/html')
+	
+	except KeyError:
+		formin = form('name','pass','register')
+		return web.Response(text=formin,content_type='text/html')
+
+
+
+
+
+
+
+app = web.Application()
+app.add_routes([
+                web.get('/register', register),
+                
+                ])
+
+
+web.run_app(app)
