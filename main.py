3ef11c812f01fcbe71213ee93d8745b17474921e
bigjango13 <bigjango13@gmail.com>
2022-02-17 23:29:15 -0500

    Added basic post veiwing

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from aiohttp import web
 from os.path import exists
 from cryptography.fernet import Fernet
 
+def getPost(id):
+    return(json.load(open("json/posts.json"))[str(id)])
+
 def form(name,desc,location):
 	form = open('elements/form.html','r')
 	formout = form.read()
@@ -42,12 +45,33 @@ async def register(request):
             userwrite.write(json.dumps(data, indent=2))
             print('The user"'+name+'" was added.')
             return web.Response(text='yay',content_type='text/html')
-	
+
         return web.Response(text='something went wrong sorry',content_type='text/html')
 
     except KeyError:
         formin = form('name','pass','register')
         return web.Response(text=formin,content_type='text/html')
+
+async def main(request):
+    posts = ""
+    for id in range(len(json.load(open("json/posts.json", "r")).keys())):
+        currentPost = getPost(id)
+        posts += f"""<div class=post>
+  <p class=name rows="2" readonly>
+    "<b>{currentPost["title"]}</b>" {"&#x1F512;" if currentPost["locked"] else ""}
+    <br> posted by <b>{currentPost["author"]}</b>
+  </p>
+  <textarea readonly class=textpost>
+    {currentPost["content"]}
+  </textarea>
+  <br>
+    <a href=/comments?id={id}>
+      <button>comments (don't work yet)</button>
+    </a>
+  </br>
+</div>"""
+    page = open("index.html").read().replace("^posts^", posts)
+    return web.Response(text=page, content_type='text/html')
 
 # Generates the key if it doesn't exist
 if not exists('secret.key'):
@@ -61,6 +85,9 @@ f = Fernet(key)
 # Starts the web app
 app = web.Application()
 app.add_routes([
-                web.get('/register', register),
-    ])
+    web.get('/register', register),
+    web.get('/index', main),
+    web.get('/', main)
+])
 web.run_app(app)
+
